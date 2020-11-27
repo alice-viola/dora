27c9440b0f420adcb6e7d4514709270d8e53b033
nicolapego <nicola.peghini@gmail.com>
2020-11-27 09:31:02 +0100

    form: done, gpuworkload selected, no more WEB workload

--- app/src/plugins/vuetify.js
+++ app/src/plugins/vuetify.js
@@ -8,13 +8,18 @@ import Vuetify, {
 	VSelect,
 	VFileInput,
 	VCombobox,
-	VBtn
+	VBtn,
+	VIcon,
 } from 'vuetify/lib';
 import { Ripple, Intersect, Touch, Resize } from 'vuetify/lib/directives';
+import '@mdi/font/css/materialdesignicons.css'
 
 Vue.use(Vuetify, {
-	components: { VRow, VTooltip, VCol, VTextField, VCheckbox, VSelect, VFileInput, VCombobox, VBtn},
+	components: { VRow, VTooltip, VCol, VTextField, VCheckbox,
+		VSelect, VFileInput, VCombobox, VBtn, VIcon},
 	directives: { Ripple, Intersect, Touch, Resize },
+	icons: {iconfont: 'mdi' },// || 'mdiSvg' || 'md' || 'fa' || 'fa4' || 'faSvg'
+
 });
 
 
