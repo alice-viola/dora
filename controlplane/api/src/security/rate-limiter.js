2437d24e9846a7fc35aa37c55083c90b13678356
Amedeo Setti <amedeosetti@gmail.com>
2021-01-04 16:06:33 +0100

    Increased rate limiter

--- controlplane/api/src/security/rate-limiter.js
+++ controlplane/api/src/security/rate-limiter.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 
 let opts = {
 	keyPrefix: process.env.rateLimiterPrefix || 'ratelimiter',
-  	points: process.env.rateLimiterPoints || 10,
+  	points: process.env.rateLimiterPoints || 30,
   	duration: process.env.rateLimiterDuration || 1,
 }
   
